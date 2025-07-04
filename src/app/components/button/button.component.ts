a32049d4f59fed091c779788815cc4d738d2d56e
Bectaly13 <dodocad13@gmail.com>
2025-07-04 14:27:22 +0200

    better buttons

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -10,7 +10,6 @@ export class ButtonComponent {
   title = input.required<string>();
   subtitle = input.required<string>();
   path = input.required<string>();
-  iconPath = input.required<string>();
 
   constructor(private router: Router) { }
 
