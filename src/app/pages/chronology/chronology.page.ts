9000d18cf17df719e59e6be1b3b20996164a0cd8
Bectaly13 <dodocad13@gmail.com>
2025-07-08 11:37:08 +0200

    chronology beta

--- src/app/pages/chronology/chronology.page.ts
+++ src/app/pages/chronology/chronology.page.ts
@@ -1,20 +1,87 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, ViewWillEnter, IonButton } from '@ionic/angular/standalone';
+import { DragDropModule, CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
+
+import { ChampionsService, Champion } from 'src/app/services/champions.service';
+import { StorageService } from 'src/app/services/storage.service';
+
+import { NavbarComponent } from 'src/app/components/navbar/navbar.component';
 
 @Component({
   selector: 'app-chronology',
   templateUrl: './chronology.page.html',
   styleUrls: ['./chronology.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, CommonModule, FormsModule, NavbarComponent, IonButton, DragDropModule]
 })
-export class ChronologyPage implements OnInit {
+export class ChronologyPage implements ViewWillEnter {
+  champions: Champion[] = [];
+  n: number = 0;
 
-  constructor() { }
+  slots: (Champion | null)[] = [null, null, null, null, null];
+  draggables: Champion[] = [];
 
-  ngOnInit() {
+  showResult: boolean = false;
+  won: boolean = false;
+
+  constructor(private champ: ChampionsService,
+              private storage: StorageService) { }
+
+  ionViewWillEnter(): void {
+    this.getChampionsData();
+
+    this.startGame();    
   }
 
+  getChampionsData() {
+    this.champions = this.champ.champions;
+    this.n = this.champions.length;
+  }
+
+  async startGame() {
+    const draggables_data = await this.storage.get("chronology_draggables_data");
+
+    if(!draggables_data) {
+      this.resetGame();
+    }
+
+    else {
+      this.draggables = draggables_data;
+    }
+  }
+
+  resetGame() {
+    const shuffledChampions = [...this.champions].sort(() => Math.random() - 0.5);
+    this.draggables = shuffledChampions.slice(0, 4);
+    this.storage.set("chronology_draggables_data", this.draggables);
+
+    this.slots = Array(this.draggables.length).fill(null);
+    this.showResult = false;
+    this.won = false;
+  }
+
+  reorder(event: CdkDragDrop<Champion[]>) {
+    moveItemInArray(this.draggables, event.previousIndex, event.currentIndex);
+  }
+
+  submit() {
+    this.showResult = true;
+
+    const years = this.draggables.map(champ => champ.year);
+    const sorted = [...years].sort((a, b) => a - b);
+    this.won = JSON.stringify(years) === JSON.stringify(sorted);
+
+    this.saveStats();
+    this.clearData();
+  }
+
+  saveStats() {
+
+  }
+
+  clearData() {
+    this.storage.remove("chronology_draggables_data");
+  }
 }
