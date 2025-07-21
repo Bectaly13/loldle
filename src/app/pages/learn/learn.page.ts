3be3a264a26eb76c2a7902dd690a0588363f4069
Bectaly13 <dodocad13@gmail.com>
2025-07-21 15:40:10 +0200

    learn loading fix (??)

--- src/app/pages/learn/learn.page.ts
+++ src/app/pages/learn/learn.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, ViewWillEnter, IonButton, IonItem, IonLabel, IonList, IonCard, IonCardContent } from '@ionic/angular/standalone';
@@ -15,13 +15,43 @@ import { NavbarComponent } from 'src/app/components/navbar/navbar.component';
   imports: [IonContent, CommonModule, FormsModule, IonButton, IonItem, IonLabel, IonList, IonCard, IonCardContent, NavbarComponent]
 })
 export class LearnPage implements ViewWillEnter {
+  @ViewChild('contentRef', { static: false }) contentRef!: IonContent;
+
   champions: Champion[] = this.champ.champions;
   selectedChampion: Champion | null = null;
+  loadedChampions: Champion[] = [];
+  batchSize: number = 50;
+  currentIndex: number = 0;
 
   constructor(private champ: ChampionsService) { }
 
   ionViewWillEnter(): void {
+    this.loadedChampions = [];
+    this.currentIndex = 0;
+    this.loadMoreChampions();
     this.selectedChampion = null;
+  }
+
+  loadMoreChampions() {
+    if (this.currentIndex >= this.champions.length) return;
+    const nextBatch = this.champions.slice(this.currentIndex, this.currentIndex + this.batchSize);
+    this.loadedChampions = this.loadedChampions.concat(nextBatch);
+    this.currentIndex += this.batchSize;
+  }
+
+  async onScroll() {
+    const scrollEl = await this.contentRef.getScrollElement();
+    
+    const scrollTop = scrollEl.scrollTop;
+    const clientHeight = scrollEl.clientHeight;
+    const scrollHeight = scrollEl.scrollHeight;
+
+    const threshold = 200;
+    const position = scrollTop + clientHeight;
+
+    if (position + threshold >= scrollHeight) {
+      this.loadMoreChampions();
+    }
   }
 
   selectChampion(champion: Champion) {
